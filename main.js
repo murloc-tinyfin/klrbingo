3089d4dd75d7e3038a6ecb0192056c0c91297305
murloc-tinyfin <147312648+murloc-tinyfin@users.noreply.github.com>
2025-12-28 15:35:42 +0000

    Add files via upload
    Initial file upload

---
+++ main.js
@@ -0,0 +1,103 @@
+// categories for bingo spaces
+const generic_songs = ["A theme from crystal shards", "A theme from super star (ultra)", "A theme from mass attack",
+                       "A theme from return to dream land (deluxe)", "A theme from (extra) epic yarn", 
+                       "A theme from triple deluxe", "A theme from planet robobot", 
+                       "A theme from star allies", "A theme from forgotten land"/*mainline titles*/,
+                       "A theme from air ride",  "A theme from rainbow curse", "A theme from (extra) epic yarn",
+                       "A theme from fighters 2", "A theme from team kirby clash deluxe/ super kirby clash"/*non-mainline titles*/];
+
+const specific_songs = ["Green green", "CROWNED", "Mind in a program", "Gourmet race", 
+                        "You just got ALIVEL MALLED", "HEAVY LOBSTER",
+                        "Meta Knight's Revenge", "King Dedede's Theme", "Planet popstar 64",
+                        "Strongest warrior in the galaxy", "Zero 2", "Moonstruck blossom"];
+                    
+const albums = ["An azifly theme", "A theme from orchestra", "A theme from Kirbtunes", "A theme from kirby rip attack"];
+
+const generic_gameplay = ["adventure gameplay", "dream land 2 gameplay", "super star (ultra) gameplay", "dream land 3 gameplay", 
+                  "crystal shard gameplay", "amazing mirror gameplay", "squeak squad gameplay", "return to dream land (deluxe) gameplay",
+                  "triple deluxe gameplay", "planet robobot gameplay", "star allies gameplay", "forgotten land gameplay"/*mainline titles*/,
+                  "air ride gameplay", "(extra) epic yarn gameplay", "rainbow curse gameplay"/*non-mainline titles*/];
+
+//const specific_gameplay = [];  (left empty for now)
+
+const generic_themed_songs = ["a grass theme", "a water theme", "a space theme",  "a sky theme", "a desert theme", 
+                              "a forest theme", "a mechanical theme", "a volcano theme", "a beach theme", "a wind theme"];
+
+const specific_themed_songs = ["a theme from one of the 4 knights", "a final boss theme", "favourite theme playing", "a theme from the game in gameplay",
+                               "a mini boss theme", "a remixed theme from previous games", "2 theme in a row from the same game"];
+
+const miscellaneous = ["MARX JUMPSCARE (anything marx related show up)", "Get 300 xp in chat", "The parasol kirbies line up", 
+                       "@poyostar appearence", "@kirbisbestpoyo appearence", "@hi_im_awkward appearance", "@kahomapler appearance",
+                       ">15 people in chat", "<5 people in chat", "background change", "chat spam(more than 5 similar messages)",];
+
+
+
+// function to shuffle an array
+function shuffleArray(array) {  
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+}
+
+// function to pick n random items from an array
+// used to pick out the items from the different categories
+function pickRandomItems(array, n) {
+
+    let shuffled = [...array];
+
+    shuffled = shuffleArray(shuffled);
+
+    return shuffled.slice(0, n);
+}
+
+// function to generate bingo board
+let first_time = true;
+function generate_bingo_board() {
+    if (!first_time) {
+    // confirm with the user before generating a new board
+    // Star allies style layers :)
+    let proceed = confirm("Generate a new bingo board?");
+    if (!proceed) {return;}
+
+    proceed = confirm("Are you sure?");
+    if (!proceed) {return;}
+
+    proceed = confirm("Are you really, really sure?");
+    if (!proceed) {return;}
+
+    proceed = confirm("This can't be undone. Are you REALLY sure?");
+    if (!proceed) {return;}
+
+    }else{first_time = false;}
+
+    // pick the corrsponding number of random items from each category
+    const shuffled_generic_songs = pickRandomItems(generic_songs, 4);
+    const shuffled_specific_songs = pickRandomItems(specific_songs, 5);
+    const shuffled_albums = pickRandomItems(albums, 1);
+    const shuffled_generic_gameplay = pickRandomItems(generic_gameplay, 3);
+    const shuffled_generic_themed_songs = pickRandomItems(generic_themed_songs, 3);
+    const shuffled_specific_themed_songs = pickRandomItems(specific_themed_songs, 5);
+    const shuffled_miscellaneous = pickRandomItems(miscellaneous, 4);
+
+    // combine all the picked items into one array and shuffle them
+    let combined = [...shuffled_generic_songs, ...shuffled_specific_songs, ...shuffled_albums, ...shuffled_generic_gameplay,
+                    ...shuffled_generic_themed_songs, ...shuffled_specific_themed_songs, ...shuffled_miscellaneous];
+    combined = shuffleArray(combined);
+
+    // generate the bingo board
+    let tableHTML = '<table border="1" style="border-collapse: collapse;"><thead><tr><th>K</th><th>I</th><th>R</th><th>B</th><th>Y</th></tr></thead>';
+    for (let i = 0; i < 5; i++) {
+        tableHTML += '<tr>';
+        for (let j = 0; j < 5; j++) {
+            tableHTML += `<td style="padding: 5px;">${combined[i * 5 + j]}</td>`;
+        }
+    tableHTML += '</tr>';
+    }
+    tableHTML += '</table>';
+
+    // display the bingo board
+    document.getElementById("bingo-board").innerHTML = tableHTML;
+}
